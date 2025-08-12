5f568ffa9ed6b8aa3cffc26160946570c8c3705a
lovishgoyal-05 <lovishgoyal112@gmail.com>
2025-08-13 00:43:35 +0530

    assignment 1

---
+++ assignment 1/Q1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	cout << "Hello World" << endl;
+	cout << "This is a new line using \\n\nNext line." << endl;
+	cout << endl << "This is a new line using endl." << endl;
+	cout << "This\tis\ta\ttabbed\tmessage." << endl;
+	cout << "Alarm sound: \a" << '\a' << endl;
+	cout << "Carriage return example: Start\rEnd " << endl;
+	return 0;
+}
